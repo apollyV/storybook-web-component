f447e9cfc0a1a2a98231af32eda0773ca8142d01
Constant VENNIN <cvennin@unis.fr>
2023-08-21 11:48:06 +0200

    publish

---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+// rollup.config.mjs
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { babel } from '@rollup/plugin-babel';
+
+import { createRequire } from 'node:module';
+const requireFile = createRequire(import.meta.url);
+const packageJson = requireFile('./package.json');
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve({
+        extensions: ['.js'],
+      }),
+      commonjs(),
+      terser(),
+      babel({
+        extensions: ['.js'],
+        exclude: 'node_modules/**',
+      }),
+    ],
+    external: [],
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export * from './counter';
+export * from './stories/Button';
+export * from './stories/Header';
+export * from './stories/Page';
